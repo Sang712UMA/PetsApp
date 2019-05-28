0df9c91d8525b782fa17c9429a2ef5053e438ec0
Konstantina Marantidou <K0nstantina@users.noreply.github.com>
2019-05-28 11:18:47 +0300

    Created classes foundpets, lostpets, "clean" the class IPost

--- Source code/petsapp/src/main/java/org/halkidiki/petsapp/IPost.java
+++ Source code/petsapp/src/main/java/org/halkidiki/petsapp/IPost.java
@@ -3,54 +3,7 @@ package org.halkidiki.petsapp;
 import java.util.Date;
 
 public class IPost {
-	private Date foundDate;
-	private String foundLocation;
-	Pet FoundPet1 = new Pet();
-	private Date lostDate;
-	private String lostLocation;
 	
-	public IPost(){}
-	
-	public IPost(Date foundDate,String foundLocation){
-		this.foundDate = foundDate;
-		this.foundLocation = foundLocation;
-	}
-	public IPost(String lostLocation,Date lostDate){
-		this.lostDate = lostDate;
-		this.lostLocation = lostLocation;
-	}
-
-	public Date getFoundDate() {
-		return foundDate;
-	}
-
-	public void setFoundDate(Date foundDate) {
-		this.foundDate = foundDate;
-	}
-
-	public String getFoundLocation() {
-		return foundLocation;
-	}
-
-	public void setFoundLocation(String foundLocation) {
-		this.foundLocation = foundLocation;
-	};
-	
-	public Date getLostDate() {
-		return lostDate;
-	}
-
-	public void setLostDate(Date lostDate) {
-		this.lostDate = lostDate;
-	}
-
-	public String getLostLocation() {
-		return lostLocation;
-	}
-
-	public void setLostLocation(String lostLocation) {
-		this.lostLocation = lostLocation;
-	}
 	
 	
 }
