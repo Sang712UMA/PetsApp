be8a0a636aaa20115025e846a02d89e6a62e8aa0
antek1111 <38079612+antek1111@users.noreply.github.com>
2019-05-29 13:53:43 +0200

    Added volunteer and task lists to Shelter class

--- Source code/petsapp/src/main/java/org/halkidiki/petsapp/Shelter.java
+++ Source code/petsapp/src/main/java/org/halkidiki/petsapp/Shelter.java
@@ -1,25 +1,30 @@
 package org.halkidiki.petsapp;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author antek
  */
 public class Shelter extends Account{
     
+    private ArrayList<Volunteer> shelterVolunteers;
+    private ArrayList<Task> shelterTasks;
+
     public Shelter(){
-        
+
     }
-    
+
     public ArrayList<Volunteer> listVolunteers(){
-        return new ArrayList<Volunteer>();
-    }    
-    
-    public void sendMessage(String text){
-        
+        return shelterVolunteers;
     }
-    
+
+    public void sendMessage(String text){
+
+    }
+
     public void sendTask(ArrayList<Task> taskList){
-        
+
     }
     
 }
