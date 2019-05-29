1a9d025decdfa1702e768d8d83d830735742234f
antek1111 <38079612+antek1111@users.noreply.github.com>
2019-05-29 21:21:39 +0200

    Implement Volunteer class

--- Source code/petsapp/src/main/java/org/halkidiki/petsapp/Volunteer.java
+++ Source code/petsapp/src/main/java/org/halkidiki/petsapp/Volunteer.java
@@ -1,5 +1,29 @@
 package org.halkidiki.petsapp;
 
-public class Volunteer extends User {
+import java.util.ArrayList;
 
+/**
+ *
+ * @author antek
+ */
+public class Volunteer extends User{
+    
+    private ArrayList<FreeHours> freeHoursList;
+    
+    public ArrayList<FreeHours> getFreeHours(){
+        return freeHoursList;
+    }
+    
+    public void addFreeHours(FreeHours freeHours){
+        this.freeHoursList.add(freeHours);
+    }
+    
+    public boolean removeFreeHours(FreeHours freeHours){
+        return this.freeHoursList.remove(freeHours);
+    }
+    
+    public void sendMessage(String text){
+        
+    }
+    
 }
