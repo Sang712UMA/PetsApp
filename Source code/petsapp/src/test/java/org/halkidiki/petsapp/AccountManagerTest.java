265b67a51046bff204556c62f8541f065ab43bd1
Adrián Salas <45205815+adrisalas@users.noreply.github.com>
2019-05-29 14:05:05 +0200

    TDD

---
+++ Source code/petsapp/src/test/java/org/halkidiki/petsapp/AccountManagerTest.java
@@ -0,0 +1,14 @@
+package org.halkidiki.petsapp;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class AccountManagerTest {
+
+	@Test
+	final void test() {
+		fail("Not yet implemented"); // TODO
+	}
+
+}
