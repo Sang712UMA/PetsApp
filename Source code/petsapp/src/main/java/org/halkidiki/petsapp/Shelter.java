63ff38179c2a5604d4d117494de613406d4ef15f
antek1111 <38079612+antek1111@users.noreply.github.com>
2019-05-29 21:37:41 +0200

    Added management of volunteers and tasks in Shelter class

--- Source code/petsapp/src/main/java/org/halkidiki/petsapp/Shelter.java
+++ Source code/petsapp/src/main/java/org/halkidiki/petsapp/Shelter.java
@@ -17,7 +17,27 @@ public class Shelter extends Account{
     
     public ArrayList<Volunteer> listVolunteers(){
         return shelterVolunteers;
-    }    
+    }
+    
+    public void addVolunteer(Volunteer volunteer){
+        this.shelterVolunteers.add(volunteer);
+    }
+    
+    public boolean removeVolunteer(Volunteer volunteer){
+        return this.shelterVolunteers.remove(volunteer);
+    }
+    
+    public ArrayList<Task> listTasks(){
+        return shelterTasks;
+    }
+    
+    public void addTask(Task task){
+        this.shelterTasks.add(task);
+    }
+    
+    public boolean removeTask(Task task){
+        return this.shelterTasks.remove(task);
+    }
     
     public void sendMessage(String text){
         
