bd4ee6b7239cf74bcc84df1a87d503c1fd3d6232
Adrián Salas <45205815+adrisalas@users.noreply.github.com>
2019-05-29 13:19:46 +0200

    Adding classes to github

---
+++ Source code/petsapp/src/main/java/org/halkidiki/petsapp/TaskNotification.java
@@ -0,0 +1,17 @@
+package org.halkidiki.petsapp;
+
+public class TaskNotification implements INotification {
+
+	@Override
+	public User getUser() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean doPostMustBeNotified(Pet pet) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
