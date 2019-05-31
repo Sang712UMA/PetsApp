24a99938f6f7655135115c573952039f96a879ec
Adrián Salas <45205815+adrisalas@users.noreply.github.com>
2019-05-31 23:00:57 +0200

    NotificationManager done using TDD

--- Source code/petsapp/src/main/java/org/halkidiki/petsapp/notifications/NotificationManager.java
+++ Source code/petsapp/src/main/java/org/halkidiki/petsapp/notifications/NotificationManager.java
@@ -1,5 +1,7 @@
 package org.halkidiki.petsapp.notifications;
 
+import java.util.ArrayList;
+
 /**
  * 
  * @Author: Adrian
@@ -7,7 +9,6 @@ package org.halkidiki.petsapp.notifications;
  */
 
 import java.util.List;
-
 import org.halkidiki.petsapp.post.Post;
 
 public class NotificationManager {
@@ -16,23 +17,60 @@ public class NotificationManager {
 	private List<Notification> activeNotifications;
 
 	private NotificationManager() {
+		activeNotifications = new ArrayList<Notification>();
 	}
-	
+
 	public static NotificationManager getActiveNotificationManager() {
-		return null;
+		if (activeNotificationManager == null) {
+			activeNotificationManager = new NotificationManager();
+		}
+		return activeNotificationManager;
 	}
 
 	public void addNotification (Notification notification) {
+		activeNotifications.add(notification);
+	}
+
+	public List<Notification> getNotificationsForAPost (Post post) {
+		List<Notification> notificationsToSend = new ArrayList<Notification>();
 		
+		for(Notification notification : activeNotifications) {
+			if (isAPetNotification(notification)) {
+				if (((PetNotification)notification).doPostMustBeNotified(post.getPet())) {
+					notificationsToSend.add(notification);
+				}
+			}
+		}
+		
+		return notificationsToSend;
 	}
 
-	public void checkIfPostMustAlertUser (Post post) {
-	}
-
+	
 	public void deleteNotification (Notification notification) {
+		activeNotifications.remove(notification);
+	}
+	
+	public void sendNotification(PetNotification petNotification, Post post) {
+		sendNotification(petNotification);
+		System.out.println("Maybe this is the pet you are looking for: " + post.getPet());
+	}
+	
+	public void sendNotification(MessageNotification message) {
+		sendNotification(message);
+		System.out.println(message.message);
+	}
+	
+	private void sendNotification(Notification notification) {
+		//TODO (TCP: SERVER -> CLIENT)
+		System.out.println("NOTIFY USER " + notification.getUser() + " : ");
+		
 	}
 
-	private void sendNotification (Notification notification) {
-		
+	private boolean isAPetNotification (Notification notification) {
+		return notification instanceof PetNotification;
+	}
+	/*GETTERS*/
+	public List<Notification> getNotifications() {
+		return activeNotifications;
 	}
 }
