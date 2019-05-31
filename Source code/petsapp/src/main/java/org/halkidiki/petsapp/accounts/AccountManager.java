55f0b6503a52b9b0a314812c92977e91543b3615
Adrián Salas <45205815+adrisalas@users.noreply.github.com>
2019-05-31 18:02:02 +0200

    Update AccountManager skeleton for Sanggil

--- Source code/petsapp/src/main/java/org/halkidiki/petsapp/accounts/AccountManager.java
+++ Source code/petsapp/src/main/java/org/halkidiki/petsapp/accounts/AccountManager.java
@@ -1,5 +1,7 @@
 package org.halkidiki.petsapp.accounts;
 
+import java.util.List;
+
 /**
  * 
  * @Author: Adrian
@@ -7,5 +9,35 @@ package org.halkidiki.petsapp.accounts;
  */
 
 public class AccountManager {
+	
+	List<User> activeUsers;
+	List<Administrator> activeAdministrators;
+	List<Shelter> activeShelters;
+	private static AccountManager activeAccountManager=null;
+	
+	private AccountManager() {
+	}
 
+	public static AccountManager getActiveAccountManager(){
+		return null;
+	}
+	
+	public List<User> getUsers() {
+		return null;
+	}
+	
+	public List<Administrator> getAdministrator(){
+		return null;
+	}
+	
+	public List<Shelter> getShelters(){
+		return null;
+	}
+	
+	public void addAccount(Account account) {
+	}
+
+	public void deleteAccount(Account account) {
+	
+	}
 }
