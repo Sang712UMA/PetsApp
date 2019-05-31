aaf68118a8031d02f1f200920e40b9c956dabc4c
Sang712UMA <48283397+Sang712UMA@users.noreply.github.com>
2019-05-31 19:06:22 +0200

    Modifing

--- Source code/petsapp/src/main/java/org/halkidiki/petsapp/accounts/Administrator.java
+++ Source code/petsapp/src/main/java/org/halkidiki/petsapp/accounts/Administrator.java
@@ -1,5 +1,8 @@
 package org.halkidiki.petsapp.accounts;
 
+import org.halkidiki.petsapp.App;
+import org.halkidiki.petsapp.Contest;
+
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
@@ -7,22 +10,21 @@ import java.util.List;
 /*
  * by Sanggil
  */
-import org.halkidiki.petsapp.Reward;
 
-public class Administrator extends User{
+public class Administrator extends Account{
 	
 	
-	Reward reward;
+	Contest contest;
 	Administrator(){}
-	public Reward getReward() {
-		return reward;
+	public Contest getContest() {
+		return contest;
 	}
-	public void setReward(Reward reward) {
-		this.reward = reward;
+	public void setContest(Contest contest) {
+		this.contest = contest;
 	}
-	public Administrator(Reward reward) {
+	public Administrator(Contest contest) {
 		super();
-		this.reward = reward;
+		this.contest = contest;
 	}
 	
 	User user;
@@ -36,22 +38,23 @@ public class Administrator extends User{
 		super();
 		this.user = user;
 	}
-	
-	public static List<User> timeRanker = new ArrayList<User>();
-//	public void checkTotaltime() {
+//	AccountManager AM = AccountManager.getActiveAccountManager();
+//	public List<User> users = AM.getUsers();
+//	private void checkTotaltime() {
 //		String output = null;
-//		List<User> eligibleUser = Account.activeUsers;
-//		for(User user : eligibleUser) {
+//		List<User> timeRanker = new ArrayList<User>();
+//		int[] rank = new int[users.size()] ;
+//		for(int i = 0; i<users.size(); i++) {
+//			rank[i] = users i.getTimeDevoted();
 //			
-//			if (eligibleUser.)
 //		}
-//		
 //	}
+		
 	
 	public void setEvent(Date startdate, Date enddate) {
-		 reward.seteStarted(true);
-		 reward.seteStartDate(startdate);
-		 reward.seteEndDate(enddate);
+		 contest.setcStarted(true);
+		 contest.setcStartDate(startdate);
+		 contest.setcEndDate(enddate);
 	}
 	
 //	public String setWinner(String nickname) {
@@ -69,4 +72,45 @@ public class Administrator extends User{
 //	}
 	
 	
+	static void heapify(int[] array, int length, int i) {  
+	    int leftChild = 2*i+1;
+	    int rightChild = 2*i+2;
+	    int largest = i;
+
+	    // if the left child is larger than parent
+	    if (leftChild < length && array[leftChild] > array[largest]) {
+	        largest = leftChild;
+	    }
+
+	    // if the right child is larger than parent
+	    if (rightChild < length && array[rightChild] > array[largest]) {
+	        largest = rightChild;
+	    }
+
+	    // if a swap needs to occur
+	    if (largest != i) {
+	        int temp = array[i];
+	        array[i] = array[largest];
+	        array[largest] = temp;
+	        heapify(array, length, largest);
+	    }
+	}
+
+	public static void heapSort(int[] array) {  
+	    if (array.length == 0) return;
+
+	    // Building the heap
+	    int length = array.length;
+	    // we're going from the first non-leaf to the root
+	    for (int i = length / 2-1; i >= 0; i--)
+	        heapify(array, length, i);
+
+	    for (int i = length-1; i >= 0; i--) {
+	        int temp = array[0];
+	        array[0] = array[i];
+	        array[i] = temp;
+
+	        heapify(array, i, 0);
+	    }
+	}
 }
