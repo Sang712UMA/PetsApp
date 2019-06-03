877a336ac7ff4e3e6c260018d86dad6141f4d292
halikidiki <mazurkiewiczpw@gmail.com>
2019-06-04 01:03:22 +0200

    no message

--- Source code/petsapp/src/main/java/org/halkidiki/petsapp/post/Petsitting.java
+++ Source code/petsapp/src/main/java/org/halkidiki/petsapp/post/Petsitting.java
@@ -1,20 +1,23 @@
 package org.halkidiki.petsapp.post;
 
+import java.time.LocalDate;
 import java.util.Date;
 
 import org.halkidiki.petsapp.accounts.Account;
 
 public class Petsitting extends Post {
 
+	LocalDate startDate;
 	private int duration;
 	public static int rewardPoints;
 
 	public Petsitting() {
 	}
 
-	public Petsitting(int duration, int rewardPoints) {
+	public Petsitting(int duration, int rewardPoints,LocalDate startDate) {
 		this.duration = duration;
 		this.rewardPoints = rewardPoints;
+		this.startDate = startDate;
 	}
 
 	public int getDuration() {
@@ -31,6 +34,10 @@ public class Petsitting extends Post {
 
 	public void setRewardPoints(int rewardPoints) {
 		this.rewardPoints = rewardPoints;
+	}
+	
+	public void setStartDate(LocalDate startDate) {
+		this.startDate = startDate;
 	}
 
 }
